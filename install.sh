994a069002ca9af0eff2d316418163a283ad0858
Ethan <ethanjhem.e@gmail.com>
2018-02-24 00:20:01 +0000

    Updated python installation section

--- install.sh
+++ install.sh
@@ -19,19 +19,22 @@ while [ $secs -gt 0 ]; do
 done
 
 #Creates temp files and at the end removes them unless terminated
-cd ~/ || echo "error changing directory"
+cd ~/ || echo "error changing directory" && exit 1
 mkdir Temporary
 cd ~/Temporary || echo "error changing directory" && exit 1
 
 #Gets dependencies
 wget --quiet --https-only -O python.7z https://ecloud.zapto.org/index.php/s/e443rdQyzNxJmTK/download?path=%2F&files=python.7z
-sudo apt-get install gnome-screensaver
 
 #Installs dependencies
 sudo apt-get install p7zip-full
 7z x python.7z
 cd python || echo "error changing directory" && exit 1
-./configure --enable-optimizations
-make
-sudo make install
-echo "Installation Complete! please run bluetooth-unlock.py" && exit 0
+echo "Do you wish to install python 3? (recommended but will take time!)"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) ./configure --enable-optimizations && make && sudo make install && echo "Installation Complete! please run bluetooth-unlock.py" && exit 0;;
+        No ) rm ~/Temporary && echo "Installation Complete! please run bluetooth-unlock.py" && exit 0;;
+        * ) echo "Invalid Choice"
+    esac
+done
