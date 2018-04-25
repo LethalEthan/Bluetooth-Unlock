c087c24163a24763b38b9fdd055a332f39258d55
Ethan <ethanjhem.e@gmail.com>
2018-04-25 21:27:03 +0100

    Added Distro support
    Now can detect to use apt or yum, WARNING: yum may not work I couldn't test it due to VirtualBox not working so I added a few more commands to hopefully make it work please tell me in the issue section if it does not work. Apt has been tested and is working

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ fi
 
 echo "============================================="
 echo "Bluetooth Unlock Tool For Linux Distributions"
-echo "            Setup Version V.4.2              "
+echo "            Setup Version V.5.1              "
 echo "============================================="
 
 #10 second countdown timer
@@ -18,13 +18,35 @@ while [ $secs -gt 0 ]; do
    sleep 1
    : $((secs--))
 done
-
-#Installs python dependencies 
-
-sudo apt install -y bluetooth libbluetooth-dev || bluedepfail=1
-
-sudo -H -u $USER python3 -m pip install pybluez || bluedepfail=1
-
-if [[ $bluedepfail == "1" ]]; then
-    echo "Installing python blueooth dependencies failed!"
+release=$(lsb_release -i) || echo "lsb_release not found"
+release2=$(gentoo_release -i) || echo "gentoo_release not found"
+echo $release
+if [[ $release == "Distributor ID:	Ubuntu" ]]; then
+  echo "Using apt"
+  sudo apt update
+  sudo apt install -y python3 python3-pip bluetooth libbluetooth-dev || aptinstallfail=1
+  sudo -H -u $USER python3 -m pip install pybluez || bluedepfail=1
+  if [[ $aptinstallfail == "1" ]]; then
+    echo "Apt has failed"
+  elif [[ $bluedepfail == "1" ]]; then
+    echo "Pip has failed"
+  fi
+  echo "Install complete! please run Bluetooth-Unlock.py"
+elif [[ $release == "Distributor ID:	Fedora" ]]; then
+  echo "Using yum, WARNING THIS MAY NOT WORK!"
+  yum install -y python3 python3-pip bluetooth libbluetooth-dev || aptinstallfail=1
+  sudo -H -u $USER python3 -m pip install pybluez || bluedepfail=1
+elif [[ $release2 == "Distributor ID:	Gentoo" ]]; then
+  echo "Using yum, WARNING THIS MAY NOT WORK!"
+  yum install -y python3 python3-pip bluetooth libbluetooth-dev || aptinstallfail=1
+  sudo -H -u $USER python3 -m pip install pybluez || bluedepfail=1
 fi
+#Installs python dependencies
+
+#sudo apt install -y bluetooth libbluetooth-dev || bluedepfail=1
+
+#sudo -H -u $USER python3 -m pip install pybluez || bluedepfail=1
+
+#if [[ $bluedepfail == "1" ]]; then
+#    echo "Installing python blueooth dependencies failed!"
+#fi
