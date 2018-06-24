ad8c7f27d6c1c2dea7b10c470b1a5ef34080c99d
Ethan <ethanjhem.e@gmail.com>
2018-06-24 20:00:35 +0100

    Fully working script (might be added to stable)
    Quite a lot of changes:
    -Fixed grammatical errors
    -Shortened names of variables
    -The text has got structure and you have time to read it
    -Removed some unnecessary text
    -Pinging the device doesn't fail as much because the checkinterval is now being used at the start
    -You may have to re-enter your device address due to the variable name change but no big deal (will still work but if you want you can delete the deviceaddr bit in config.ini)
    -When the return code is over 0 it will lock so if the return code is 10 it will still lock just in case (better security basically)
    -Added more comments to show what is going on (more to be done)
    -The contribution list is now displayed even without debug mode (mistake)
    -Now says when it's active (for non-debug mode)
    -More things to be done such as a GUI (Currently being worked on, but having some issues), Better distro compatibility in install.sh etc
    -I will also default the Y/N to No when the Unknown response is triggered so you don't have to start from the beginning

--- Bluetooth-Unlock.py
+++ Bluetooth-Unlock.py
@@ -18,9 +18,6 @@ except:
     print("Cannot import the bluetooth modules!")
     print("Please run install.sh!")
     sys.exit(1)
-#Gets OS path for upcoming auto-update feature
-dir_path = os.path.dirname(os.path.realpath(__file__))
-print(dir_path)
 
 #Detects python version
 if (sys.version_info > (3, 0)):
@@ -38,10 +35,10 @@ config.read("config.ini")
 
 #Variables
 USER = getpass.getuser() #For the thank you message
-GET_DEVICEADDR = 1 #For setup when no config is found
+GET_DEVADDR = 1 #For setup when no config is found
 SELECT_ENV = 1 #For setup when no config is found
 
-#Checks for a new version, i've tried to make it automatically install but due to many problems it had to be postponed
+#Checks for a new version
 if config.has_option("VERSION", "version"):
     VERSION = config.get("VERSION", "version")
 else:
@@ -67,6 +64,8 @@ if UPDATE == "Y":
                 print("Downloading Update from ecloud...")
                 os.system("wget -q -O Update.tar.gz https://ecloud.zapto.org/index.php/s/7CM8NtHjZfDr9KS/download")
                 print("Downloaded!, please extract new files in current directory!")
+            else:
+                print("Not downloading update")
         elif NEWVERSION < VERSION:
             config.clear()
             print("Version installed is higher than the version specified in update config!")
@@ -81,20 +80,24 @@ elif UPDATE == "N":
 else:
     print("Unknown response!")
 config.read("config.ini")
-
+time.sleep(2)
 #Loads the options from the config and loads them into a local variable
+#Loads Desktop Environment option
 if config.has_option("DESKTOP", "env"):
-    print("Desktop Environment found in config, using the one specified!")
+    print("\nDesktop Environment found, using:")
     SELECT_ENV = 0
     ENV = config.get("DESKTOP", "env")
-if config.has_option("DEVICEADDR", "deviceaddr"):
-    print("Device Adress found in config, using the one specified!\n")
-    GET_DEVICEADDR = 0
-    DEVICEADDR = config.get("DEVICEADDR", "deviceaddr")
-if config.has_option("DESKTOP", "env"):
-    print("Desktop Environment found in config, using one specified!\n")
-else:
-    AVAILDE = input("Would you like to see the available desktop environments? [Y/N]")
+    print(ENV)
+#Loads Device Address
+if config.has_option("DEVADDR", "devaddr"):
+    print("\nDevice Address found, using:")
+    GET_DEVADDR = 0
+    DEVADDR = config.get("DEVADDR", "devaddr")
+    print(DEVADDR)
+time.sleep(3)
+#User can see what Desktop Environments are available (not all are detected)
+if SELECT_ENV == 1:
+    AVAILDE = input("\nWould you like to see the available desktop environments? [Y/N]")
     AVAILDE = AVAILDE.upper()
     if AVAILDE == "Y":
             gnome_screensaver = {'GNOME': 'gnome-screensaver'}
@@ -118,9 +121,11 @@ else:
             print("")
     elif AVAILDE == "N":
         print("Not displaying the available desktop environments")
+    else: ("Uknown response")
 #Select Desktop Environment menu
 if SELECT_ENV == 1:
     #Detects if these desktop evironments are available
+    print("")
     ENV = input("""Please Enter your Desktop Environment can be:
     "LOGINCTL" (Recommended) (Don't use sudo)
     "KDE" (Doesn"t work on older versions)
@@ -131,33 +136,28 @@ if SELECT_ENV == 1:
     """)
     ENV = ENV.upper()
     if ENV == "LOGINCTL":
-        print(ENV,"has been selected")
+        print(ENV,"has been selected\n")
     elif ENV == "KDE":
-        print(ENV,"has been selected")
+        print(ENV,"has been selected\n")
     elif ENV == "GNOME":
-        print(ENV,"has been selected")
+        print(ENV,"has been selected\n")
     elif ENV == "XSCREENSAVER":
-        print(ENV,"has been selected")
+        print(ENV,"has been selected\n")
     elif ENV == "MATE":
-        print(ENV,"has been selected")
+        print(ENV,"has been selected\n")
     elif ENV == "CINNAMON":
-        print(ENV,"has been selected")
+        print(ENV,"has been selected\n")
     else:
         sys.exit("Unidentified Environment exiting")
     config["DESKTOP"] = {"ENV": (ENV)}
     with open("config.ini", "w") as configfile:
         config.write(configfile)
 
-elif SELECT_ENV == 0:
-    print("Config found using specified desktop environment")
-    print(ENV)
-    print("")
-
-print("Bluetooth-Unlock is a free open-source project and always will be")
+print("\nBluetooth-Unlock is a free open-source project and always will be")
 print("I would like you to consider donating to allow me to do other projects")
 print("Thanks :)\n")
 
-#Debug mode prints extra information of what"s going on
+#Debug mode prints extra information of what's going on
 DEBUG = input("Would you like to activate debug mode? [Y/N]")
 DEBUG = DEBUG.upper()
 if DEBUG == "Y":
@@ -168,29 +168,33 @@ else:
 	sys.exit("Unknown option")
 
 #Code containing thank you message and device detection
+print("")
 print("Thank you for using Bluetooth-Unlock",VERSION ,USER ,"\n")
-if GET_DEVICEADDR == 1:
+if GET_DEVADDR == 1:
     print("Searching for nearby devices...\n")
     nearby = discover_devices(lookup_names = True)
     print(nearby,"\n")
-    DEVICEADDR = input("Enter Bluetooth Adress of the device (e.g AA:BB:CC:DD:EE:FF): ")#Asks for bluetooth device address
-    config["DEVICEADDR"] = {"DEVICEADDR": (DEVICEADDR)}
+    DEVADDR = input("Enter Bluetooth Address of the device (e.g AA:BB:CC:DD:EE:FF): ")#Asks for bluetooth device address
+    config["DEVADDR"] = {"DEVADDR": (DEVADDR)}
     with open("config.ini", "w") as configfile:
         config.write(configfile)
-elif GET_DEVICEADDR == 0:
-    print("Device Address is", DEVICEADDR)
+elif GET_DEVADDR == 0:
+    print("Device Address is", DEVADDR)
+time.sleep(2)
 #Prints information
 if DEBUG == "Y":
-    print("This is the sections found in config.ini")
+    print("\nThis is the sections found in config.ini")
     print(config.sections())
     print("Desktop Environment is", ENV)
-    print("Device Address is",DEVICEADDR,"\n")
-    print("Thank you to these contributors: jose1711, maaudrana, blackPanther OS")
-    print("jose1711 has improved the code of this project")
-    print("maaudrana is making a logo for this project")
-    print("blackPanther OS has improved/optimized the code of this project")
-    print("Thanks to all of them :)\n")
-
+    print("Device Address is",DEVADDR,"\n")
+    time.sleep(4)
+else: time.sleep(1)
+print("\nThank you to these contributors: jose1711, maaudrana, blackPanther OS")
+print("jose1711 has improved the code of this project")
+print("maaudrana is making a logo for this project")
+print("blackPanther OS has improved/optimized the code of this project")
+print("Thanks to all of them :)\n")
+time.sleep(4)
 #Variables for Main code
 CHECKINTERVAL = 3 # device pinged at this interval (seconds) when screen is unlocked min:3
 CHECKREPEAT = 2  # device must be unreachable this many times to lock
@@ -199,28 +203,28 @@ mode = "unlocked"
 #Main code for Bluetooth-Unlock
 #Return code 0 is when the command has ran successfully
 #Return code 1 is when the command has failed to reach the device
+print("Bluetooth-Unlock is now active!")
 while True:
     tries = 0
     while tries < CHECKREPEAT:
-        check = subprocess.Popen(["sudo", "l2ping", DEVICEADDR, "-t", "1", "-c", "1"], shell=False, stdout=subprocess.PIPE)
+        #Checks for the device
+        time.sleep(CHECKINTERVAL)
+        check = subprocess.Popen(["sudo", "l2ping", DEVADDR, "-t", "1", "-c", "1"], shell=False, stdout=subprocess.PIPE)
         check.wait()
         retcode = check.returncode
         if retcode == 0 and DEBUG == "Y":
             print("ping OK!")
             break
-        else:
+        elif retcode > 0 and DEBUG == "Y":
+            #Prints returncode when debug is active
             print("ping response code: %d" % (retcode))
             time.sleep(1)
             tries = tries + 1
 
-            if retcode > 0:
-                print("Child was terminated by signal", retcode)
-            elif DEBUG == "Y":
-                print("Child returned", retcode)
-
         if retcode == "Can\'t create socket: Operation not permitted":
             print("Couldn't create a socket due to permissions")
 
+    #Unlocks when the device IS found
     if retcode == 0 and mode == "locked":
         mode = "unlocked"
         if ENV == "LOGINCTL":
@@ -236,7 +240,8 @@ while True:
         elif ENV == "CINNAMON":
             os.system("cinnamon-screensaver-command -d")
 
-    if retcode == 1 and mode == "unlocked":
+    #Locks when the device ISN'T be found
+    if retcode > 0 and mode == "unlocked":
         mode = "locked"
         if ENV == "LOGINCTL":
             os.system("loginctl lock-session")
